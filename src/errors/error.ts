e75d8d7669344669d52fda5caa5e89b85fa7914a
Ronald Torrez <dev@ronaldtorrez.com>
2025-03-27 02:00:40 -0400

    feat: add modern-errors plugins and clean up deps
    - Add modern-errors-bugs and modern-errors-clean
    - Remove unused zod-accelerator

--- src/errors/error.ts
+++ src/errors/error.ts
@@ -1,8 +1,12 @@
 import ModernError from 'modern-errors'
+import modernErrorsBugs from 'modern-errors-bugs'
+import modernErrorsClean from 'modern-errors-clean'
 import modernErrorsWinston from 'modern-errors-winston'
 
 export const BaseError = ModernError.subclass( 'BaseError', {
-    plugins: [ modernErrorsWinston ]
+    plugins: [ modernErrorsWinston, modernErrorsBugs, modernErrorsClean ]
 } )
 
-export const UnknownError = BaseError.subclass( 'UnknownError' )
+export const UnknownError = BaseError.subclass( 'UnknownError', {
+    bugs: 'https://github.com/larafriend/tmerge/issues'
+} )
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { installCommand } from '@/commands/install.command'
 import { mergeCommand } from '@/commands/merge.command'
 import { unmergeCommand } from '@/commands/unmerge.command'
+import { UnknownError } from '@/errors/error'
+import LoggerInstance from '@/logger/logger'
 import { Command } from 'commander'
+import { Logger } from 'winston'
 import package_ from '../package.json'
 
 const program = new Command()
@@ -16,3 +19,8 @@ program.addCommand( mergeCommand )
 program.addCommand( unmergeCommand )
 
 program.parse()
+
+const logger: Logger = LoggerInstance.get( {} )
+const error = new UnknownError( 'Test error', { props: { test: 'test' } } )
+
+logger.error( error )
